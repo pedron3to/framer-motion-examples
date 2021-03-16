72dfbc592cfe1a6de4d693a5b455050d02803c84
pedron3to <62304426+pedron3to@users.noreply.github.com>
2021-03-16 15:26:53 -0300

    teste

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Headline from "../sections/landing-page/Headline"
+
+function Home() {
+  return (
+    <>
+      <Headline/>
+      <div> text</div>
+      <div> text</div>
+      <div> text</div>
+      <div> text</div>
+      <div> text</div>
+      <div> text</div>
+
+    </>
+  )
+}
+
+export default Home
--- sections/landing-page/Headline.js
+++ sections/landing-page/Headline.js
@@ -1,16 +1,38 @@
 import React, { useContext } from "react";
+import Image from 'next/image'
 import styled from "styled-components";
 import { Title, Section, Box, Text } from "../../components/Core";
 import { device } from "../../utils";
 
+const BgWrap = styled.div`
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  z-index: -1;
+`
 
+const HeadlineWrapper = styled.div`
+  position: absolute;
+  top: 70px;
+  right: 15px;
+`
 const Headline = () => {
   return (
     <>
-      <Section bg="#fff" className="vh-100">
-        <div>
-          <div>
-            <div>
+     
+      <BgWrap>
+         <Image
+          alt="Mountains"
+          src="/background.jpg"
+          layout="fill"
+          objectFit="none"
+          quality={100}
+        />       
+
+      </BgWrap>
+         
+            <HeadlineWrapper>
               <Title variant="hero" className="mb-3 text-center">
                 Aqui vai a sua Headline.
 
@@ -19,14 +41,13 @@ const Headline = () => {
                 Utilize a subheadline para complementar a sua headline com
                 informações que facilitem o entendimento do produto ou serviço.
               </Text>
-            </div>
+            </HeadlineWrapper>         
+            
+     
+          
 
-          </div>
-          <div>
-
-          </div>
-        </div>
-      </Section>
+          
+    
     </>
   );
 };
